c6625d4b9b40f003f412a00d3f5199dbbf74e45a
Adnan <adnankhurshid251@gmail.com>
2022-02-19 18:47:42 +0530

    before making venv

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR/"staticfiles",
+    BASE_DIR/"static",
 ]
 
 MEDIA_ROOT = BASE_DIR/"uploads"
