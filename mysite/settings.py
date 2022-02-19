15eb0c7bd6fc4964883575acbcf99c6dac6f2e0e
Adnan <adnankhurshid251@gmail.com>
2022-02-19 19:10:10 +0530

    afer adding collecstatic urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
 
-STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = BASE_DIR/"uploads"
